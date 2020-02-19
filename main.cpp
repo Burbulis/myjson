3affb1d11356db00abc9ab3e7b74a412ca6c20f9
Konstantin Chernickevich <onehalf3554@mail.ru>
2020-02-19 23:56:20 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "scope.hpp"
+int main(int argc,char **argv)
+{
+
+
+}
---
+++ scope.hpp
@@ -0,0 +1,32 @@
+namespace Scope
+{
+
+struct StartTokens
+{
+  StartTokens()
+  {}
+
+};
+
+struct EndTokens
+{
+  EndTokens()
+  {}
+
+};
+
+
+
+template
+<
+  typename ScopeStart,
+  typename ScopeEnd
+>
+struct Scope
+{
+ Scope()
+ {}
+
+};
+
+}
