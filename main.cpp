7b8f0cb8e870a18642603366b07c314a3c62014f
Konstantin Chernickevich <onehalf3554@mail.ru>
2020-02-20 23:18:29 +0300

    generate tokenization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "scope.hpp"
 int main(int argc,char **argv)
 {
+Scope::StartTokens_c ss_('{');
+Scope::EndTokens_c se_('}');
 
+ Scope::Detector<Scope::StartTokens_c,Scope::EndTokens_c> scope_(ss_,se_);
 
 }
--- scope.hpp
+++ scope.hpp
@@ -1,32 +1,47 @@
 namespace Scope
 {
 
+template
+<
+  typename T
+>
 struct StartTokens
 {
-  StartTokens()
+  StartTokens(const T& t_)
   {}
 
 };
 
+typedef StartTokens<char> StartTokens_c;
+
+template
+<
+  typename T
+>
 struct EndTokens
 {
-  EndTokens()
+  EndTokens(const T& t_)
   {}
 
 };
 
+typedef EndTokens<char> EndTokens_c;
 
 
 template
 <
-  typename ScopeStart,
-  typename ScopeEnd
+  typename TScopeStart,
+  typename TScopeEnd
 >
-struct Scope
+struct Detector
 {
- Scope()
+ Detector(const TScopeStart& lvalue,const TScopeEnd& rvalue):
+ _ss(lvalue),_se(rvalue)
  {}
-
+ 
+  private:
+  TScopeStart _ss;
+  TScopeEnd   _se;
 };
 
 }
