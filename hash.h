c6fa9842c94ec06ec795952765bbdd1add92755c
Burbulis <onehalf3554@mail.ru>
2020-10-01 16:25:55 +0300

    Add files via upload

---
+++ hash.h
@@ -0,0 +1,11 @@
+
+
+#ifndef __HASHER__
+#define __HASHER__
+#include <math.h>
+#include <stdio.h>
+
+  unsigned long get_hash(unsigned long index);
+  unsigned long rehasher(unsigned long x_,unsigned long count_,char token_);
+  
+#endif
